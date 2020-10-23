c837d5528027f1dd6fe440302b0459c6de94960b
yjchen <yi-jen.chen@gmx.de>
2020-10-23 17:51:16 +0200

    Update Ch8 solutions

--- 08-numbers/08_2.py
+++ 08-numbers/08_2.py
@@ -3,10 +3,11 @@ def check_divisible(num1, num2):
     
     remainder = num1 % num2
 
-    if remainder == 0:
-        print(f'{num1} is divisible by {num2}')
-    else:
+    if remainder:
         print(f'{num1} is not divisible by {num2}')
+    else:
+        print(f'{num1} is divisible by {num2}')
+        
 
 check_divisible(297, 3)
 check_divisible(297, 0)
