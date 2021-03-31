8ab48226cca2c7a107e6ed20f927c03d4958e91c
yjchen <yi-jen.chen@gmx.de>
2021-03-31 16:09:03 +0200

    complete ch.11

--- 11_strings_and_methods/11_1.py
+++ 11_strings_and_methods/11_1.py
@@ -1,5 +1,5 @@
 school = 'Long-An'
 
 print(dir(school))
-print('*' * 60)
-print(help(school.swapcase))
+print('\n')
+help(school.swapcase)
--- 11_strings_and_methods/11_3.py
+++ 11_strings_and_methods/11_3.py
@@ -1,5 +1,5 @@
 filename = 'hello.py'
 
 print(filename.endswith('.java'))
-print(filename.find('py'))  # also: str.index()
+print(filename.find('py'))  # alternative: .index()
 print(filename.startswith('world'))
